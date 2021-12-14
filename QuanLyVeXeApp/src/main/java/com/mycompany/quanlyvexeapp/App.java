e45eabdcaf67e28940f4adda71064d8e92253faf
LEHOANGGLAM <lelam1384@gmail.com>
2021-12-14 14:48:08 +0700

    import depenacy for test

--- QuanLyVeXeApp/src/main/java/com/mycompany/quanlyvexeapp/App.java
+++ QuanLyVeXeApp/src/main/java/com/mycompany/quanlyvexeapp/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("VeXe"));
+        scene = new Scene(loadFXML("ChuyenDi"));
         stage.setScene(scene);
         stage.show();
     }
--- QuanLyVeXeApp/src/main/java/module-info.java
+++ QuanLyVeXeApp/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.mycompany.quanlyvexeapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires java.base;
+    requires java.base;  
 
     opens com.mycompany.quanlyvexeapp to javafx.fxml;
     exports com.mycompany.quanlyvexeapp;
